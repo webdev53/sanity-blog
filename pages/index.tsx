95032f85b08ae5b45764558fbd89aeeac54ac96d
Web Dev <web.2019.dev@gmail.com>
2022-05-01 15:19:36 +0545

    homepage added

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,29 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <div className="items-center justify-between border-y border-black bg-blue-300 py-10 lg:py-10">
+        <div className="space-y-5 px-10">
+          <h1 className="max-w-xl font-serif text-6xl">
+            This{' '}
+            <span className="underline decoration-black decoration-4">
+              Blog
+            </span>{' '}
+            platform is a place to write, read and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
+          </h2>
+        </div>
+      </div>
     </div>
   )
 }
