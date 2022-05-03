d520364527a6b5329c074e5d690bc6e1c4691582
Web Dev <web.2019.dev@gmail.com>
2022-05-03 22:35:43 +0545

    comment functionality added

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,12 +2,47 @@ import { GetStaticProps } from 'next'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
+import PortableText from 'react-portable-text'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { useState } from 'react'
+
+interface IFormInput {
+  _id: string
+  name: string
+  email: string
+  comment: string
+}
 
 interface Props {
   post: Post
 }
 
 function Post({ post }: Props) {
+  const [submitted, setSubmitted] = useState(false)
+
+  console.log(post)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    await fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data)
+        setSubmitted(true)
+      })
+      .catch((err) => {
+        console.log(err)
+        setSubmitted(false)
+      })
+  }
+
   return (
     <main>
       <Header />
@@ -37,7 +72,106 @@ function Post({ post }: Props) {
             at {new Date(post._createdAt).toLocaleString()}
           </p>
         </div>
+
+        <div className="mt-10">
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
       </article>
+
+      <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
+
+      {submitted ? (
+        <div className="my-10 mx-auto flex max-w-2xl flex-col bg-blue-500 py-10 pl-10 text-white">
+          <h3 className="text-3xl font-bold">
+            Thank you for submitting your comment
+          </h3>
+          <p>Once it has been approved, it will appear below</p>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
+        >
+          <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+          <hr className="mt-2 py-3" />
+
+          <input
+            type="hidden"
+            {...register('_id')}
+            name="_id"
+            value={post._id}
+          />
+
+          <label className="mb-5 block">
+            <span className="text-gray-700">Name</span>
+            <input
+              {...register('name', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+              placeholder="Your Name"
+              type="text"
+            />
+          </label>
+          <label className="mb-5 block">
+            <span className="text-gray-700">Email</span>
+            <input
+              {...register('email', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+              placeholder="Your Email"
+              type="email"
+            />
+          </label>
+          <label className="mb-5 block">
+            <span className="text-gray-700">Comment</span>
+            <textarea
+              {...register('comment', { required: true })}
+              className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring "
+              placeholder="Your Comment"
+              rows={8}
+            />
+          </label>
+          {/* errors will show if required validation fails(name, email, comment) */}
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-500">The Name field is required</span>
+            )}
+            {errors.comment && (
+              <span className="text-red-500">
+                The comment field is required
+              </span>
+            )}
+            {errors.email && (
+              <span className="text-red-500">The Email field is required</span>
+            )}
+          </div>
+          <input
+            type="submit"
+            className="focus:shadow-outline cursor-pointer rounded bg-blue-500 py-2 px-4 font-bold text-white shadow hover:bg-blue-400 focus:outline-none"
+            name=""
+            id=""
+          />
+        </form>
+      )}
     </main>
   )
 }
